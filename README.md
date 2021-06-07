87f1ad4f541cdba240cac899ea1b307cd47f312c
Luminous <luminous@heavenark.com>
2021-06-07 20:59:18 +0800

    fix(Wrapper): Fix Promise Invoke Error
    - fixed promise invoke error
    - deleted readme part

--- README.md
+++ README.md
@@ -26,9 +26,9 @@ Universal queue SDK
 
 ### Single Function list:
 
-- [ ] Rabbit ConfirmChannel Support
+- [x] Rabbit ConfirmChannel Support
 - [ ] Rabbit Manager Api Full Support
-- [ ] Set Max Channel Count When Creating Connection
+- [x] Set Max Channel Count When Creating Connection
 
 ## Onirii Supported Features List
 
@@ -43,77 +43,4 @@ Universal queue SDK
 | Kafka |  |  |  |  |  |  |  |  |  |
 | MQTT |  |  |  |  |  |  |  |  |  |
 | RocketMQ |  |  |  |  |  |  |  |  |  |
-| RabbitMQ | ✓ | ✓ | ✓ | ✓ |  |  |  |  |  |
-
-## Developing Status
-
-Note:: ✓ realized ✓✓ realized and tested
-
-### OriginalQueueInterface
-
-| Queue | createQueue | getQueue | deleteQueue | purgeQueue |
-| :----: | :-----: | :----: | :----: | :----: |
-| RabbitMQ | x | x | ✓✓ | ✓✓ | 
-
-### AmqpQueueInterface
-
-| Queue | assertQueue | getQueueStatus | bindQueue | unbindQueue |
-| :----: | :-----: | :----: | :----: | :----: |
-| RabbitMQ | ✓✓ | ✓✓ | ✓✓ | ✓✓ |
-
-### AmqpExchangeInterface
-
-| Queue | assertExchange | checkExchange | deleteExchange | bindExchange | unbindExchange |  
-| :----: | :-----: | :----: | :----: | :----: |  :----: |
-| RabbitMQ | ✓✓ | ✓✓ | ✓✓ | ✓✓ | ✓✓ |
-
-## SDK Description
-
-### AmazonSQS Generic AWS-SDK (Queue Sdk Version: 2012-11-05)
-
-method return type: (params = {}, callback)
-
-#### Queue
-
-| SDK | Onirii | Description |
-| ---- | ---- | ---- |
-| createQueue() | |Creates a new standard or FIFO queue. |
-| setQueueAttributes() | | Sets the value of one or more queue attributes. |
-| listQueues() | | Returns a list of your queues in the current region. |
-| getQueueUrl() | | Returns the URL of an existing Amazon SQS queue. |
-| getQueueAttributes() | | Gets attributes for the specified queue. |
-| deleteQueue() | | Deletes the queue specified by the QueueUrl, regardless of the queue's contents. |
-| purgeQueue() | | Deletes the messages in a queue specified by the QueueURL parameter. |
-
-#### Message
-
-| SDK | Onirii | Description |
-| ---- | ---- | ---- |
-| sendMessage() | | Delivers a message to the specified queue. |
-| sendMessageBatch() | | Delivers up to ten messages to the specified queue. |
-| receiveMessage() | | Retrieves one or more messages (up to 10), from the specified queue. |
-| deleteMessage() | | Deletes the specified message from the specified queue. |
-| deleteMessageBatch() | | Deletes up to ten messages from the specified queue. |
-| changeMessageVisibility() | | Changes the visibility timeout of a specified message in a queue to a new value. |
-| changeMessageVisibilityBatch() | |Changes the visibility timeout of multiple messages. |
-
-#### Tag
-
-| SDK | Onirii | Description |
-| ---- | ---- | ---- |
-| tagQueue() | | Add cost allocation tags to the specified Amazon SQS queue. |
-| untagQueue() | | Remove cost allocation tags from the specified Amazon SQS queue. |
-| listQueueTags() | | List all cost allocation tags added to the specified Amazon SQS queue. |
-
-#### Permission
-
-| SDK | Onirii | Description |
-| ---- | ---- | ---- |
-| addPermission() | | Adds a permission to a queue for a specific principal. | 
-| removePermission() | | Revokes any permissions in the queue policy that matches the specified Label parameter. |
-
-#### Other
-
-| SDK | Onirii | Description |
-| ---- | ---- | ---- |
-| listDeadLetterSourceQueues() | | Returns a list of your queues that have the RedrivePolicy queue attribute configured with a dead-letter queue. |
+| RabbitMQ | ✓ | ✓ | ✓ | ✓ | ✓ | ✓ | ✓ | ✓ | ✓ |
---
+++ src/service/rabbit-enhancer.ts
@@ -0,0 +1,195 @@
+import amqp from 'amqplib';
+import { Options } from 'amqplib/properties';
+import { RMExchange } from '../model/rabbit-manager/rabbit-manager-exchange-namespace';
+import { RMQueue } from '../model/rabbit-manager/rabbit-manager-queue-namespace';
+import { AmqpEnhancerConsumerWrapper } from '../wrapper/amqp-enhancer-consumer-wrapper';
+import { AmqpChannelService } from './amqp/amqp-channel-service';
+import { AmqpConfirmChannelService } from './amqp/amqp-confirm-channel-service';
+import { AmqpConnectService } from './amqp/amqp-connect-service';
+
+export class RabbitEnhancer {
+
+  private readonly instanceName: string;
+
+  private readonly enhancerProtocol: string;
+
+  private connectionPool: AmqpConnectService[] = [];
+
+  private channelPool: (AmqpChannelService | AmqpConfirmChannelService)[] = [];
+
+  private consumerPool: AmqpEnhancerConsumerWrapper[] = [];
+
+  private readonly configure: RabbitEnhancerConfigure;
+
+  private connectionPoint = 0;
+
+  private channelPoint = 0;
+
+  constructor(name: string, protocol: 'amqp091' | 'http', configure: RabbitEnhancerConfigure) {
+    this.instanceName = name;
+    this.enhancerProtocol = protocol;
+    this.configure = configure;
+  }
+
+  public async ready() {
+    // create connection
+    for (let i = 0; i < this.configure.openConnectionCount; i++) {
+      const connectInstance = new AmqpConnectService(`${this.instanceName}`, i);
+      await connectInstance.ready();
+      if (!connectInstance) {
+        throw new Error(`Can Not Create Connection ${this.instanceName}-${i}`);
+      }
+      this.connectionPool.push(connectInstance);
+    }
+    // create channel
+    for (let i = 0; i < this.configure.openChannelCount; i++) {
+      const confirm: boolean = this.configure.openChannelType === 'confirm';
+      const currentConnection = this.getNextConnection();
+      const channelInstance = confirm ?
+        await currentConnection.createChannelService(true) :
+        await currentConnection.createChannelService(false);
+      if (!channelInstance) {
+        throw new Error(`Can Not Create Channel At ${currentConnection.instanceName}`);
+      }
+      this.channelPool.push(channelInstance);
+    }
+    // init resource
+    if (this.configure.resourceConfigure) {
+      await this.initExchange();
+      await this.initQueue();
+      await this.initBind();
+    }
+  }
+
+  public async addConsumer(
+    queue: string,
+    processor: (msg: amqp.GetMessage, channel: AmqpChannelService) => Promise<boolean>,
+    delay?: number,
+    consumerName?: string,
+    options?: Options.Get,
+  ): Promise<void> {
+    const consumer = this.getNextChannel().enhancerConsume(queue, processor, delay, consumerName, options);
+    if (consumer) {
+      this.consumerPool.push(consumer);
+    }
+  }
+
+  public async killConsumer(name: string): Promise<void> {
+    const currentConsumer = this.consumerPool.find(element => element.consumerName === name);
+    if (currentConsumer) {
+      await currentConsumer.kill();
+    }
+    this.consumerPool = this.consumerPool.filter(element => element.consumerName !== name);
+  }
+
+  public async pushOperation(processor: (currentChannel: AmqpChannelService) => Promise<any>): Promise<any> {
+    return await processor(this.getNextChannel());
+  }
+
+  public async close(): Promise<void> {
+    for (let amqpConnectService of this.connectionPool) {
+      await amqpConnectService.close();
+    }
+  }
+
+
+  private async initExchange(): Promise<void> {
+    const exchangeInitData = this.configure.resourceConfigure!.exchangeList;
+    if (!exchangeInitData) {
+      return;
+    }
+    for (let exchangeInitDatum of exchangeInitData) {
+      if (this.enhancerProtocol === 'amqp091') {
+        await this.getNextChannel()
+          .createExchangeIfNotExist(exchangeInitDatum.name, exchangeInitDatum.type, exchangeInitDatum.options);
+      }
+    }
+  }
+
+  private async initQueue(): Promise<void> {
+    const queueInitData = this.configure.resourceConfigure!.queueList;
+    if (!queueInitData) {
+      return;
+    }
+    for (let queueInitDatum of queueInitData) {
+      if (this.enhancerProtocol === 'amqp091') {
+        await this.getNextChannel().createQueueIfNotExist(queueInitDatum.name, queueInitDatum.options);
+      }
+    }
+  }
+
+  private async initBind(): Promise<void> {
+    const bindListData = this.configure.resourceConfigure!.bindList;
+    if (!bindListData) {
+      return;
+    }
+    for (let bindListDatum of bindListData) {
+      if (bindListDatum.type === 'qte') {
+        await this.bindQueueToExchange(bindListDatum);
+      } else if (bindListDatum.type === 'ete') {
+        await this.bindExchangeToExchange(bindListDatum);
+      }
+    }
+  }
+
+  private async bindQueueToExchange(configure: RabbitEnhancerBind) {
+    if (this.enhancerProtocol === 'amqp091') {
+      await this.getNextChannel().bindQueueToExchange(configure.targetSourceName, configure.fromSourceName, configure.key, configure.options);
+    }
+  }
+
+  private async bindExchangeToExchange(configure: RabbitEnhancerBind) {
+    if (this.enhancerProtocol === 'amqp091') {
+      await this.getNextChannel().bindExchangeToExchange(configure.targetSourceName, configure.fromSourceName, configure.key, configure.options);
+    }
+  }
+
+  private getNextConnection(): AmqpConnectService {
+    if (this.connectionPoint === this.connectionPool.length) {
+      this.connectionPoint = 0;
+    }
+    const currentConnection = this.connectionPool[this.connectionPoint];
+    this.connectionPoint++;
+    return currentConnection;
+  }
+
+  private getNextChannel(): AmqpChannelService {
+    if (this.channelPoint === this.channelPool.length) {
+      this.channelPoint = 0;
+    }
+    const currentChannel = this.channelPool[this.channelPoint];
+    this.channelPoint++;
+    return currentChannel;
+  }
+
+}
+
+export interface RabbitEnhancerConfigure {
+  openConnectionCount: number;
+  openChannelCount: number;
+  openChannelType: 'normal' | 'confirm',
+  resourceConfigure?: {
+    exchangeList?: RabbitEnhancerExchange[],
+    queueList?: RabbitEnhancerQueue [],
+    bindList?: RabbitEnhancerBind[]
+  }
+}
+
+export interface RabbitEnhancerExchange {
+  name: string,
+  type: 'direct' | 'topic' | 'headers' | 'fanout' | 'match' | string,
+  options?: Options.AssertExchange | RMExchange.CreateOptions,
+}
+
+export interface RabbitEnhancerQueue {
+  name: string,
+  options?: Options.AssertQueue | RMQueue.CreateOptions,
+}
+
+export interface RabbitEnhancerBind {
+  type: 'qte' | 'ete',
+  fromSourceName: string,
+  targetSourceName: string,
+  key: string,
+  options?: any
+}
